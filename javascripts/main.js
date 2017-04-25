132b71d7a2d315ed714991463545cf01cc16906a
Thomas Fitzgerald <tpfitz42@gmail.com>
2017-04-24 21:33:37 -0500

    pushing api intro

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,29 @@
-console.log("Javascript loading");
+$(document).ready(function(){
+
+  const apiKey = "";
+
+
+  $("body").on("click", "li", (e) => {
+    // console.log(e.target.innerHTML);
+    returnPromise(e.target.innerHTML).then((data) => {
+      console.log(data);
+    }).catch((error) => {
+      console.log(error);
+    });
+  });
+
+  const returnPromise = (type) => {
+    return new Promise((resolve, reject) => {
+      $.ajax(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=+36.1627,-86.7816&radius=500&type=${type}&key=${apiKey}`).done((data) => {
+        resolve(data);
+      }).fail((error) => {
+        reject(error);
+      });
+    });
+  };
+
+
+
+
+
+});
